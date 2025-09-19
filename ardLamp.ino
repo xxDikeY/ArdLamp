54bc0c8686f8288cb36ae9586ba086a3546a9d16
xxDikeY <114901775+xxDikeY@users.noreply.github.com>
2025-09-19 12:33:48 +0300

    Update ardLamp.ino

--- ardLamp.ino
+++ ardLamp.ino
@@ -1,126 +1,142 @@
-// Насткройки
-// ------------------------------
-#define LED_NUM 120    // количество светодиодов
-#define volt 5 // ограничение ленты по вольтам
-#define miliAmp 1 // ограничение ленты по амперам
-#define stepsForLight 5 // на сколько будет изменятся яркость при вращении. Значения 0-100; 100 / stepsForLight = доступное количество яркости
-#define stepsForColor 5 // на сколько шагов будет менятся свет при вращении. Значения 0-255; 255 / stepsForColor = доступное количество цветов
+// -------------------- НАСТРОЙКИ ----------------------------------------------------------------------------------------
 
-int lightLevel = 50; // начальный уровень света при первом запуске. Значения 0-100;
-int colorLevel = 0; // начальный цвет при первом запуске. Значения 0-255;
-// ------------------------------
+#define LED_NUM 100 // кол-во светодиодов
 
-#define EB_FAST_TIME 10000
+// настройка цветового режима
+int colorLevel = 0; // начальный цвет (0-255)
+int colorStep = 5; // шаг изменения цвета (желательно, чтобы было кратно 5)
 
-#define CLK 3
-#define DT 4
-#define SW 5
+int lightLevel = 255; // начальная яркость (0-255)
+int lightStep = 5; // шаг изменения яркости (желательно, чтобы было кратно 5)
 
-#define LED_PIN 6
+// настройки пламени
+#define HUE_GAP 21      // заброс по hue
+#define FIRE_STEP 15    // шаг огня
+#define HUE_START 0   // начальный цвет огня (0 красный, 80 зелёный, 140 молния, 190 розовый)
+#define MIN_BRIGHT 70   // мин. яркость огня
+#define MAX_BRIGHT 255  // макс. яркость огня
+#define MIN_SAT 245     // мин. насыщенность
+#define MAX_SAT 255     // макс. насыщенность
+#define COLOR_DEBTH 2   // цветовая глубина: 1, 2, 3 (в байтах)
 
-#include <EncButton.h>
+// -----------------------------------------------------------------------------------------------------------------------
+
+#define LED_PIN 6   
+
 #include <FastLED.h>
+#include <EncButton.h>
 
-EncButton enc(CLK, DT, SW);
 CRGB leds[LED_NUM];
+EncButton eb(2, 3, 4);
 
-bool isBright = true;
-bool turnOn = true;
+
+int whiteLevel = 0;
+
+bool isOn = true;
 bool colorMode = true;
+bool colorLed = true;
+
+int counter = 0;
 
 void setup() {
-    FastLED.addLeds< WS2812, LED_PIN, GRB>(leds, LED_NUM);
-
-    Serial.begin(9600);
-    FastLED.clear();
-    FastLED.show();
+    // Serial.begin(9600);
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUM);
     FastLED.setBrightness(lightLevel);
-    
+    for (int i = 0; i < LED_NUM; i++) {
+        leds[i].setHSV(0, 255, 255);
+    }
+    FastLED.show();
 }
 
-
 void loop() {
-    enc.tick();
+    eb.tick();
 
+    if(isOn){
+        if(colorLed){
+            if (eb.turn()){
+                if (eb.right()){
+                    if(colorMode){
+                        colorLevel = (colorLevel + colorStep > 255 + colorStep) ? 0 - colorStep : colorLevel + colorStep;
+                    }
+                    else{
+                        lightLevel = (lightLevel >= 255) ? 0 + lightStep: lightLevel + lightStep;
+                    }
+                    // Serial.print("right: ");
+                }
+                else{
+                    if(colorMode){
+                        colorLevel = (colorLevel - colorStep < 0 - colorStep) ? 255 + colorStep: colorLevel - colorStep;
+                    }
+                    else{
+                        lightLevel = (lightLevel - lightStep <= 0) ? 255 : lightLevel - lightStep;
+                    }
+                    // Serial.print("left ");
+                }
+                whiteLevel = (colorLevel + colorStep > 255 + colorStep || colorLevel - colorStep < 0 - colorStep) ? 0 : 255;
+                // Serial.print(colorLevel);
+                // Serial.print(" ");
+                // Serial.print(whiteLevel);
+                // Serial.print(" ");
+                // Serial.println(lightLevel);
+                for (int i = 0; i < LED_NUM; i++) {
+                    leds[i].setHSV(colorLevel, whiteLevel, 255);
+                }
+                FastLED.setBrightness(lightLevel);
+                FastLED.show();
+            }
 
-    if (turnOn) {
-        if (enc.hold()) {
-            turnOn = false;
+            if(eb.getClicks() == 2){
+                colorLed = false;
+            }
+            else if(eb.click()){
+                if(colorMode){
+                    colorMode = false;
+                }
+                else{
+                    colorMode = true;
+                }
+            }
+        }
+        else{
+            static uint32_t prevTime;
+            int val = 0;
+            int h = 0;
+            int s = 0;
+            int v = 0;
+            if (millis() - prevTime > 20) {
+                prevTime = millis();
+                int thisPos = 0, lastPos = 0;
+                for (int i = 0; i < LED_NUM; i++) {
+                    val = (inoise8(i * FIRE_STEP, counter));
+                    h = colorLevel + map(val, 0, 255, 0, HUE_GAP);
+                    s = constrain(map(val, 0, 255, MAX_SAT, MIN_SAT), 0, 255);
+                    v = constrain(map(val, 0, 255, MIN_BRIGHT, MAX_BRIGHT), 0, 255);
+
+                    leds[i].setHSV(h, s, v);
+                }
+                counter += 20;
+                FastLED.show();
+            }
+            if(eb.click()){
+                colorLed = true;
+                for (int i = 0; i < LED_NUM; i++) {
+                    leds[i].setHSV(colorLevel, whiteLevel, 255);
+                }
+                FastLED.show();
+                colorMode = colorMode ? false : true;
+            }
+        }
+        if(eb.holding()){
+            isOn = false;
             FastLED.setBrightness(0);
-        }
-
-        if (colorMode) {
-            for (int i = 0; i < LED_NUM; i++) {
-                leds[i].setHue(colorLevel);
-            }
-        }
-
-        else {
-            for (int i = 0; i < LED_NUM; i++) {
-                leds[i] = CRGB(0, 0, 0);
-            }
             FastLED.show();
         }
-        Serial.print(colorLevel);
-        Serial.print("_");
-        Serial.println(lightLevel);
-
-        if (enc.turn()) {
-            if (isBright) {
-                if (enc.right() || enc.rightH()) {
-                    if (lightLevel + stepsForLight < 100) {
-                        if (!colorMode) {
-                            colorMode = true;
-                        }
-                        lightLevel += stepsForLight;
-                    }
-                    else colorMode = false;
-                }
-
-                else {
-                    if (lightLevel - stepsForLight > 0) {
-                        if (!colorMode) {
-                            colorMode = true;
-                        }
-                        lightLevel -= stepsForLight;
-
-                    }
-                    else colorMode = false;
-                }
-
-
-
-                FastLED.setBrightness(lightLevel);
-            }
-            else {
-                if (enc.right() || enc.rightH()) {
-                    if (colorLevel + stepsForColor < 255) colorLevel += stepsForColor;
-                    else colorLevel = 255;
-                }
-
-                else {
-                    if (colorLevel - stepsForColor > 0) colorLevel -= stepsForColor;
-                    else colorLevel = 0;
-                }
-            }
-        }
-
-        else if (enc.click()) {
-            if (isBright) {
-                isBright = false;
-            }
-
-            else {
-                isBright = true;
-            }
-        }
-
     }
-
-    else {
-        if (enc.hold()) {
-            turnOn = true;
+    else{
+        if(eb.click()){
+            isOn = true;
             FastLED.setBrightness(lightLevel);
+            FastLED.show();
         }
     }
 }
